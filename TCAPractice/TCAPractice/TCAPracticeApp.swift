9914fde3932c259da84c99b02dda9e2e93a7896d
Insu Park <facessu@gmail.com>
2023-10-08 15:25:55 +0900

    Create Project

---
+++ TCAPractice/TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by Insu Park on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
